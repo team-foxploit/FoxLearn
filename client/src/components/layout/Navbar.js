caa520c7c08ae0558d0126a2e034fb87ab770943
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2019-01-03 10:16:11 +0530

    css styles updated
    body color set

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SignedInLinks from './SignedInLinks';
-import SignedOutLinks from './SignedOutLinks';
+import React from "react";
+import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
 
 const Navbar = () => {
-    return(
-        <div className="wrapper">
-            <nav className="nav-wrapper blue lighten-2 hoverable">
-                <Link to='/' className="brand-logo">FoxLearn</Link>
-                <div className="hide-on-med-and-down">
-                    <SignedInLinks className="right" />
-                    <SignedOutLinks className="left" />
-                </div>
-            </nav>
+  return (
+    <div className="wrapper">
+      <nav className="nav-wrapper blue lighten-2 hoverable">
+        <Link to="/" className="brand-logo">
+          FoxLearn
+        </Link>
+        <div className="hide-on-med-and-down">
+          <SignedInLinks className="right" />
+          <SignedOutLinks className="left" />
         </div>
-    );
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;

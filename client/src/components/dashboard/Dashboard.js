5db415a2eb0006cb120dd47114e8dd1ca8000aab
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2019-01-03 10:16:59 +0530

    dashboard updated

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Sidebar from "./Sidebar";
 import "./styles.css";
 import Footer from "../footer/footer";
+import Wall from "./Wall";
 
 class Dashboard extends Component {
   state = {};
@@ -9,68 +10,7 @@ class Dashboard extends Component {
     return (
       <div className="dash background blue-grey">
         <Sidebar />
-        <div className="row">
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image green lighten-2">
-                <i className="material-icons md-196">multiline_chart</i>
-                <div class="card-content white">
-                  <h4>View your history</h4>
-                  <p>
-                    I am a very simple card. I am good at containing small bits
-                    of information. I am convenient because I require little
-                    markup to use effectively.
-                  </p>
-                  <div class="card-action">
-                    <button className="waves-effect waves-light btn green darken-1">
-                      My Progress
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image amber lighten-2">
-                <i className="material-icons md-196">whatshot</i>
-                <div class="card-content white">
-                  <h4>Time for a quiz</h4>
-                  <p>
-                    I am a very simple card. I am good at containing small bits
-                    of information. I am convenient because I require little
-                    markup to use effectively.
-                  </p>
-                  <div class="card-action">
-                    <button className="waves-effect waves-light btn amber darken-1">
-                      Take a quiz
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image pink lighten-2">
-                <i className="material-icons md-196">school</i>
-                <div class="card-content white">
-                  <h4>What's new?</h4>
-                  <p>
-                    I am a very simple card. I am good at containing small bits
-                    of information. I am convenient because I require little
-                    markup to use effectively.
-                  </p>
-                  <div class="card-action">
-                    <button className="waves-effect waves-light btn pink darken-1">
-                      Explore Subjects
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Wall />
         <Footer />
       </div>
     );

3602e220b3636e5103783bfb1c295c2206b1e100
luk3Sky <ganindudananja@gmail.com>
2019-01-03 09:43:23 +0530

    Quiz summary half way

--- client/src/components/quizzes/QuizList.js
+++ client/src/components/quizzes/QuizList.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import QuizDetails from './QuizDetails';
+import QuizSummary from './QuizSummary';
 
 class QuizList extends Component{
+    state = {
+        title: 'Subject',
+        author: 'author-1',
+        qid : 'id-1',
+        contentSummary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, animi facilis pariatur',
+        completeContent: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, et illum. Quos dicta aperiam cupiditate dignissimos, beatae recusandae hic, obcaecati, quam doloremque dolores laborum voluptatem molestiae. Molestiae nemo accusantium unde.'
+    }
     render(){
         return(
             <div className="quiz-list section">
+                <QuizSummary />
                 <div className="card quiz-summary">
                     <span className="card-title">Quiz Title</span>
                     <p>Posted by Someone</p>
@@ -15,12 +25,12 @@ class QuizList extends Component{
                     <p>Posted by Someone</p>
                     <p className="blue-text">Subject</p>
                 </div>
-                <div className="card quiz-summary">
-                    <span className="card-title">Quiz Title</span>
-                    <p>Posted by Someone</p>
-                    <p className="blue-text">Subject</p>
-                </div>
-                <QuizDetails />
+                {/* <BrowserRouter >
+                    <Switch >
+                        <Route path = "/:id" component={ QuizDetails } />
+                    </Switch>
+                </BrowserRouter> */}
+                <QuizDetails details = {this.state} />
             </div>
         )
     }

df4eeeba3ead43ac598949919922315b5d0722a7
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2019-01-13 14:40:08 +0530

    Form validation initial

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
-import "../layout/styles.css";
+import "./styles.css";
 import axios from "axios";
 import Footer from "../footer/footer";
-
-const emailRegex = RegExp(
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-);
 
 class SignUp extends Component {
   constructor() {
@@ -19,13 +15,26 @@ class SignUp extends Component {
       confirm_password: "",
       type: "student",
       canSubmit: false,
-      users: "",
+      users: null,
+      // errors: "",
       formErrors: {
-        username:"",
+        first_name: "",
+        last_name: "",
+        username: "",
         email: "",
-        password: ""
+        password: "",
+        confirm_password: ""
+      },
+      formValidity: {
+        first_name: false,
+        last_name: false,
+        email: false,
+        username: false,
+        password: false,
+        confirm_password: false
       }
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   async componentDidMount() {
@@ -33,61 +42,30 @@ class SignUp extends Component {
       .post("/api/users", {
         type: "usernames"
       })
-      .then(function (response) {
+      .then(function(response) {
         return response.data;
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
     console.log({ results });
-    this.setState(state => ({
-      users: results
-    }));
-    console.log(this.state);
+    this.setState(
+      {
+        users: results
+      },
+      () => {
+        for (let i = 0; i < this.state.users.length; i++) {
+          console.log(this.state.users[i].Username);
+        }
+      }
+    );
   }
-
-  handleChange = e => {
-
-    e.preventDefault();
-    const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
-
-    switch (name) {
-      case "first_name":
-        formErrors.username =
-          value.length < 3 ? "minimum 3 characaters required" : "";
-        break;
-      case "email":
-        formErrors.email = emailRegex.test(value)
-          ? ""
-          : "invalid email address";
-        break;
-      case "password":
-        formErrors.password =
-          value.length < 6 ? "minimum 6 characaters required" : "";
-        break;
-      default:
-        break;
-    }
-
-    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
-
-
-    // this.setState(
-    //   {
-    //     [e.target.id]: e.target.value
-    //   },
-    //   () => {
-    //     console.log(this.state);
-    //   }
-    // );
-  };
 
   studentSelected = () => {
     this.setState(
-      state => ({
+      {
         type: "student"
-      }),
+      },
       () => {
         console.log(this.state);
       }
@@ -105,80 +83,105 @@ class SignUp extends Component {
     );
   };
 
-  // ######################
-  validate = () => {
-    let isError = false;
-    let errors = {
-      usernameError: "",
-      emailError: "",
-      passwordError: ""
-    };
-    for (let i = 0; i < this.state.users.length; i++) {
-      if (this.state.username === this.state.users[i].Username) {
-        errors.usernameError = "This username is taken, use another one.";
-        isError = true;
-        console.log("usrname");
-      } else if (this.state.email === this.state.users[i].Email) {
-        errors.emailError = "This email address is taken, use another one.";
-        isError = true;
-        console.log("eml");
-      }
-    }
-
-    if (this.state.username.length < 5) {
-      isError = true;
-      errors.usernameError = "Username needs to be atleast 5 characters long";
-      console.log("usrln");
-    }
-
-    if (this.state.password !== this.state.confirm_password) {
-      isError = true;
-      errors.passwordError = "Password mismatch!";
-      console.log("pwd");
-    }
-
-    if (
-      this.state.email.indexOf("@") === -1 ||
-      this.state.email.indexOf(".") === -1
-    ) {
-      isError = true;
-      errors.emailError = "Email address is not valid";
-      console.log("emlntvl");
-    }
+  // triggered everytime value changes in our textboxes
+  handleChange(event) {
+    const { name, value } = event.target;
     this.setState(
-      state => ({
-        errors: errors
-      }),
-      () => {
-        console.log(errors);
-        console.log(this.state);
+      {
+        [name]: value
+      },
+      function() {
+        this.validateField(name, value);
+        console.log(name, value);
       }
     );
-    return isError;
-  };
-  // ######################
+  }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    // this.validate();
-    if (this.validate()) {
-      // cannot submit
-      // set helper error texts
-      console.log("cant");
-      console.log(this.state);
-    } else {
-      // can submit
-      // Go to sign in
-      console.log("can");
-      console.log(this.state);
+  validateField(name, value) {
+    const fieldValidationErrors = this.state.formErrors;
+    const validity = this.state.formValidity;
+    const isEmail = name === "email";
+    const isUsername = name === "username";
+    const isPassword = name === "password";
+    const isCnfrmPass = name === "confirm_password";
+
+    const emailTest = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    validity[name] = value.length > 0;
+    fieldValidationErrors[name] = validity[name]
+      ? ""
+      : `This field is required and cannot be empty`;
+
+    if (validity[name]) {
+      if (isEmail) {
+        validity[name] = emailTest.test(value);
+        fieldValidationErrors[name] = validity[name]
+          ? ""
+          : `${name} should be a valid email address`;
+      }
+      if (isPassword) {
+        validity[name] = value.length >= 6;
+        fieldValidationErrors[name] = validity[name]
+          ? ""
+          : `${name} should be 6 characters or more`;
+        if (this.state.confirm_password.length > 0) {
+          validity["confirm_password"] = this.state.confirm_password === value;
+          fieldValidationErrors["confirm_password"] = validity[
+            "confirm_password"
+          ]
+            ? "Password matches."
+            : `Password doesn't match!`;
+        }
+      }
+      if (isCnfrmPass) {
+        validity[name] = value === this.state.password;
+        fieldValidationErrors[name] = validity[name]
+          ? "Password matches."
+          : `Password doesn't match!`;
+      }
+      if (isUsername) {
+        for (let i = 0; i < this.state.users.length; i++) {
+          if (this.state.username === this.state.users[i].Username) {
+            fieldValidationErrors[name] = `'${
+              this.state.username
+            }' username already exists. Try another one!`;
+          }
+        }
+      }
     }
-    console.log(this.state);
+
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        formValidity: validity
+      },
+      () => this.canSubmit()
+    );
+  }
+
+  canSubmit() {
+    this.setState({
+      canSubmit:
+        this.state.formValidity.first_name &&
+        this.state.formValidity.last_name &&
+        this.state.formValidity.email &&
+        this.state.formValidity.username &&
+        this.state.formValidity.password &&
+        this.state.formValidity.username &&
+        this.state.formValidity.confirm_password
+    });
+  }
+
+  errorClass(error) {
+    return error.length === 0 ? "" : "is-invalid";
+  }
+
+  // triggered on submit
+  handleSubmit = event => {
+    event.preventDefault();
   };
 
   render() {
-    const { formErrors } = this.state.formErrors;
-    console.log(formErrors);
-    // console.log(this.state.errors);
     return (
       <div className="bg-img">
         <div className="row">
@@ -197,8 +200,9 @@ class SignUp extends Component {
           <div className="col s12 m4 offset-m4">
             <form
               onSubmit={this.handleSubmit}
-              className="alignCenter teal-text text-accent-1"
+              className="teal-text text-accent-1"
               id="block-2"
+              noValidate
             >
               <h4 className="teal-text text-accent-1">
                 Get statrted with FoxLearn
@@ -210,7 +214,9 @@ class SignUp extends Component {
                     id="first_name"
                     name="first_name"
                     type="text"
-                    className="validate white-text"
+                    className={`validate white-text ${this.errorClass(
+                      this.state.formErrors.first_name
+                    )}`}
                     onChange={this.handleChange}
                   />
                   <label
@@ -219,14 +225,22 @@ class SignUp extends Component {
                   >
                     First Name
                   </label>
+                  <div className="invalid-feedback">
+                    <span className="pink-text text-accent-2">
+                      {this.state.formErrors.first_name}
+                    </span>
+                  </div>
                 </div>
+
                 <div className="input-field  col s12">
                   <i className="material-icons prefix">account_circle</i>
                   <input
                     id="last_name"
                     name="last_name"
                     type="text"
-                    className="validate white-text"
+                    className={`validate white-text ${this.errorClass(
+                      this.state.formErrors.last_name
+                    )}`}
                     onChange={this.handleChange}
                   />
                   <label
@@ -235,6 +249,11 @@ class SignUp extends Component {
                   >
                     Last Name
                   </label>
+                  <div className="invalid-feedback">
+                    <span className="pink-text text-accent-2">
+                      {this.state.formErrors.last_name}
+                    </span>
+                  </div>
                 </div>
               </div>
 
@@ -245,42 +264,22 @@ class SignUp extends Component {
                     id="username"
                     name="username"
                     type="text"
-                    className="validate white-text"
+                    className={`validate white-text ${this.errorClass(
+                      this.state.formErrors.username
+                    )}`}
                     onChange={this.handleChange}
                   />
                   <label htmlFor="username" className="teal-text text-accent-2">
                     Username
                   </label>
-                  {this.state.formErrors.username.length > 0 && (
-                    <p>{this.state.formErrors.username}</p>
-                  )}
-                  {/* <span
-                    className="helper-text"
-                    data-error={this.state.errors.usernameError}
-                  >
-                    Username should be unique
-                  </span> */}
+                  <div className="invalid-feedback">
+                    <span className="pink-text text-accent-2">
+                      {this.state.formErrors.username}
+                    </span>
+                  </div>
                 </div>
               </div>
 
-              {/* <div className="row">
-                <div className="input-field col s12">
-                  <i className="material-icons prefix">account_circle</i>
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    className="validate white-text"
-                    onChange={this.handleChange}
-                  />
-                  <label htmlFor="username" className="teal-text text-accent-2">
-                    Username
-                  </label>
-                  <span className="helper-text" data-error="true">
-                    Enter username
-                  </span>
-                </div>
-              </div> */}
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">email</i>
@@ -288,15 +287,20 @@ class SignUp extends Component {
                     id="email"
                     name="email"
                     type="text"
-                    className="validate white-text"
+                    className={`validate white-text ${this.errorClass(
+                      this.state.formErrors.email
+                    )}`}
                     onChange={this.handleChange}
+                    value={this.state.email}
                   />
                   <label htmlFor="email" className="teal-text text-accent-2">
                     Email
                   </label>
-                  {/* {formErrors.length > 0 && (
-                    <span className="errorMessage">{formErrors.email}</span>
-                  )} */}
+                  <div className="invalid-feedback">
+                    <span className="pink-text text-accent-2">
+                      {this.state.formErrors.email}
+                    </span>
+                  </div>
                 </div>
               </div>
 
@@ -307,13 +311,20 @@ class SignUp extends Component {
                     id="password"
                     name="password"
                     type="password"
-                    className="validate white-text"
+                    className={`validate white-text ${this.errorClass(
+                      this.state.formErrors.password
+                    )}`}
+                    value={this.state.password}
                     onChange={this.handleChange}
                   />
                   <label htmlFor="password" className="teal-text text-accent-2">
                     Password
                   </label>
-                  {/* <span className="helper-text" data-error={this.state.errors.passwordError} /> */}
+                  <div className="invalid-feedback">
+                    <span className="pink-text text-accent-2">
+                      {this.state.formErrors.password}
+                    </span>
+                  </div>
                 </div>
               </div>
               <div className="input-field col s12">
@@ -322,7 +333,9 @@ class SignUp extends Component {
                   id="confirm_password"
                   name="confirm_password"
                   type="password"
-                  className="validate white-text"
+                  className={`validate white-text ${this.errorClass(
+                    this.state.formErrors.confirm_password
+                  )}`}
                   onChange={this.handleChange}
                 />
                 <label
@@ -331,9 +344,11 @@ class SignUp extends Component {
                 >
                   Confirm Your Password
                 </label>
-                {/* {formErrors.firstName.password > 0 && (
-                <span className="errorMessage">{formErrors.password}</span>
-              )} */}
+                <div className="invalid-feedback">
+                  <span className="lime-text text-accent-4">
+                    {this.state.formErrors.confirm_password}
+                  </span>
+                </div>
               </div>
 
               <div className="row">
@@ -372,7 +387,10 @@ class SignUp extends Component {
               </div>
               <div className="row">
                 <div className="input-field white-text">
-                  <button className="waves-effect waves-light btn deep-teal darken-3">
+                  <button
+                    className="waves-effect waves-light btn deep-teal darken-3"
+                    disabled={!this.state.canSubmit}
+                  >
                     Sign Me Up
                   </button>
                 </div>

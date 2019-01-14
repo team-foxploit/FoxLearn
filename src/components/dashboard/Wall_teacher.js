f1b8a61c8edbfa11e4684a11ca8a5aed740e283d
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2019-01-15 05:27:15 +0530

    teacher dashboard updates

--- src/components/dashboard/Wall_teacher.js
+++ src/components/dashboard/Wall_teacher.js
@@ -6,18 +6,17 @@ const WallTeacher = () => {
       <p />
       <div className="col s12 m4">
         <div className="card">
-          <div className="card-image green lighten-2">
-            <i className="material-icons md-148">multiline_chart</i>
+          <div className="card-image teal lighten-2 centText">
+            <i className="material-icons md-148">assignment_ind</i>
             <div className="card-content white">
-              <h4>View your history</h4>
+              <h4>Student Performance</h4>
               <p>
-                I am a very simple card. I am good at containing small bits of
-                information. I am convenient because I require little markup to
-                use effectively.
+                Have a glance at your students' past performance and their
+                progress.
               </p>
               <div className="card-action">
-                <button className="waves-effect waves-light btn green darken-1">
-                  My Progress
+                <button className="waves-effect waves-light btn teal darken-1">
+                  See History
                 </button>
               </div>
             </div>
@@ -26,67 +25,30 @@ const WallTeacher = () => {
       </div>
       <div className="col s12 m4">
         <div className="card">
-          <div className="card-image amber lighten-2">
-            <i className="material-icons md-148">whatshot</i>
+          <div className="card-image purple lighten-3 centText">
+            <i className="material-icons md-148">assignment</i>
             <div className="card-content white">
-              <h4>Time for a quiz</h4>
+              <h4>Create a quiz</h4>
               <p>
-                I am a very simple card. I am good at containing small bits of
-                information. I am convenient because I require little markup to
-                use effectively.
+                Have a question for students? Submit your question here and see
+                how students perform in the quizes.
               </p>
               <div className="card-action">
-                <button className="waves-effect waves-light btn amber darken-1 activator">
+                <button className="waves-effect waves-light btn purple darken-1 activator">
                   Submit a quiz
                 </button>
               </div>
             </div>
           </div>
-          <div className="card-reveal blue-grey">
-            <span className="card-title text-darken-4 white-text">
-              Select a Level<i className="material-icons right">close</i>
-            </span>
-            <div className="row">
-              <div className="card-content">
-                <div className="col s6 m6 offset-s3 offset-m3">
-                  <a
-                    className="waves-effect waves-light btn-large"
-                    id="button_easy"
-                    href="#link"
-                  >
-                    Easy
-                  </a>
-                  <a
-                    className="waves-effect waves-light btn-large"
-                    id="button_medium"
-                    href="#link"
-                  >
-                    Medium
-                  </a>
-                  <a
-                    className="waves-effect waves-light btn-large"
-                    id="button_hard"
-                    href="#link"
-                  >
-                    Hard
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <div className="col s12 m4">
         <div className="card">
-          <div className="card-image pink lighten-2">
+          <div className="card-image pink lighten-2 centText">
             <i className="material-icons md-148">school</i>
             <div className="card-content white">
               <h4>What's new?</h4>
-              <p>
-                I am a very simple card. I am good at containing small bits of
-                information. I am convenient because I require little markup to
-                use effectively.
-              </p>
+              <p>Explore latest updates on FoxLearn!</p>
               <div className="card-action">
                 <button className="waves-effect waves-light btn pink darken-1">
                   Explore Subjects

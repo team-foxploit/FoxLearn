4f5430962a42fce596c6e486d55dfe1c97f69064
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2019-01-14 22:39:16 +0530

    Teacher History | initial

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Subjects from "./components/quizzes/Subjects"; //temp routes
 import Quiz from "./components/quizzes/Quiz"; //temp routes
 import History from "./components/dashboard/History"; //temp routes
 import CreateQuiz from "./components/quizzes/CreateQuiz"; //temp routes
+import HisTeach from "./components/dashboard/History_teacher"; //temp routes
 
 class App extends Component {
   render() {
@@ -26,6 +27,7 @@ class App extends Component {
             <Route path="/quiz" component={Quiz} />
             <Route path="/his" component={History} />
             <Route path="/createquiz" component={CreateQuiz} />
+            <Route path="/histeach" component={HisTeach} />
           </Switch>
           <header className="">
             <div />

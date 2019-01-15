254d0db081601c8d8b512ff1254465202e646d5f
luk3Sky <ganindudananja@gmail.com>
2019-01-16 00:23:34 +0530

    Route Guard Basics done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import SignLanding from "./components/auth/SignLanding";
 import "./App.css";
 import Home from "./components/layout/Home";
@@ -8,21 +8,39 @@ import Dashboard from "./components/dashboard/Dashboard";
 import History from "./components/dashboard/History"; //temp routes
 import CreateQuiz from "./components/quizzes/CreateQuiz"; //temp routes
 import HisTeach from "./components/dashboard/History_teacher"; //temp routes
-import DashTeach from "./components/dashboard/Dashboard_teacher"; //temp routes
+import DashTeach from "./components/dashboard/dashboard_teacher"; //temp routes
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+    <Route  {...rest} render = {
+            (props) => {
+                const loggedInStatus = sessionStorage.getItem('isLoggedIn');
+                if (loggedInStatus === 'true') {
+                    return (
+                        <Component {...props} />
+                    )
+                }else{
+                    return (
+                        <Redirect to='/signin'/>
+                    )
+                }
+            }
+        }
+    />
+)
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home}/>
           <Route path="/signin" component={SignLanding} />
           <Route path="/signup" component={SignUp} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/his" component={History} />
-          <Route path="/createquiz" component={CreateQuiz} />
-          <Route path="/histeach" component={HisTeach} />
-          <Route path="/dashteach" component={DashTeach} />
+          <ProtectedRoute path="/dashboard" component={Dashboard} />
+          <ProtectedRoute path="/his" component={History} />
+          <ProtectedRoute path="/createquiz" component={CreateQuiz} />
+          <ProtectedRoute path="/histeach" component={HisTeach} />
+          <ProtectedRoute path="/dashteach" component={DashTeach} />
         </Switch>
       </BrowserRouter>
     );

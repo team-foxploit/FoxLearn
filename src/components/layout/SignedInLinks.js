d88d997da7abafd1f581eaf36939ed6edb661972
luk3Sky <ganindudananja@gmail.com>
2019-01-15 23:39:30 +0530

    Sign out session handle

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,14 +5,18 @@ class SignedInLinks extends Component {
 
     constructor(props){
         super(props);
-        console.log(this.props);        
+        console.log(this.props);
+    }
+
+    handleLogOut = () => {
+        sessionStorage.setItem('isLoggedIn','false');
     }
 
     render(){
         return(
             <ul className="right">
                 <li><NavLink to='/'>Discover</NavLink></li>
-                <li><NavLink to='/'>Log out</NavLink></li>
+                <li><NavLink to='/' onClick={this.handleLogOut} >Log out</NavLink></li>
                 <li><NavLink to='/' className='btn btn-floating green lighten-1'>{this.props.id}</NavLink></li>
             </ul>
         );

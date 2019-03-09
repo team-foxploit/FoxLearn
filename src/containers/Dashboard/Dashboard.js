2e56e229d37285c7a271f7c29fa953ab9a810719
luk3Sky <ganindudananja@gmail.com>
2019-03-09 23:22:54 +0530

    Dashboard initialized

---
+++ src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import Wall from '../../components/Wall/Wall';
+
+class Dashboard extends Component{
+    render () {
+        return (
+            <Wall />
+        )
+    }
+}
+
+export default Dashboard;

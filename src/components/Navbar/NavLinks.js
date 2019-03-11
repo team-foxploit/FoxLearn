58a853b4defa7079f9086b0242a1b77afb20944c
luk3Sky <ganindudananja@gmail.com>
2019-03-11 17:29:52 +0530

    Update NavLinks.js

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import propTypes from 'prop-types';
 
 const NavLinks = (props) => {
     return (
         <Aux>
             <li><a className={props.style.signLinks} href='/signup'>Sign Up</a></li>
             <li><a className={props.style.signLinks} href='/signin'>Log In</a></li>
-            <li><a className={props.style.signLinks} href='/signup'>About</a></li>
-            <li><a className={props.style.signLinks} href='/signup'>Contact</a></li>
+            <li><a className={props.style.signLinks} href='/about'>About</a></li>
+            <li><a className={props.style.signLinks} href='/contact'>Contact</a></li>
         </Aux>
     )
+}
+
+NavLinks.propTypes = {
+    syle: propTypes.object
 }
 
 export default NavLinks;

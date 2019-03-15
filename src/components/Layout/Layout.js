3ddca5e4f7e52c15e0c0b1657a3c254d6860624d
luk3Sky <ganindudananja@gmail.com>
2019-03-16 01:44:30 +0530

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+// import Navbar from '../Navbar/Navbar';
 
 const Layout = (props) => {
     return (
         <Aux>
-            <div>navbar  backdrop side drawer</div>
+            {/*<Navbar />*/}
             <main>
                 {props.children}
             </main>
